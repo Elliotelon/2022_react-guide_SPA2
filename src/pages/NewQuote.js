9fe709850acb14d835a7e52e5e9b875790caa15d
kimminkyu <kmk50009@naver.com>
2022-02-07 23:48:16 +0900

    Add UseNavigate

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,14 @@
 import QuoteForm from "../components/quotes/QuoteForm";
+import { useNavigate } from "react-router-dom";
+
 const NewQuote = () => {
+  const navigate = useNavigate();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    //history.push('/quotes')
+    navigate("/quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
