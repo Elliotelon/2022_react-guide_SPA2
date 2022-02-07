a9878161487dc54f369930eb65d7b7b2051d61ec
kimminkyu <kmk50009@naver.com>
2022-02-07 23:38:59 +0900

    Add NotFound Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
 import Layout from './components/layout/Layout';
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/quotes" element={<AllQuotes />} />
         <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
         <Route path="/new-quote" element={<NewQuote />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
   );
