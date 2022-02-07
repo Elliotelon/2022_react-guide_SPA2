8110cf5b6b096d9a558880b98807190fd13be1ce
kimminkyu <kmk50009@naver.com>
2022-02-07 22:02:00 +0900

    Add MainNavigation

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
 import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
+    <Layout>
       <Routes>
         <Route path="/" element={<Navigate to='/quotes'/>} />
         <Route path="/quotes" element={<AllQuotes />} />
         <Route path="/quotes/:quoteId/*" element={<QuoteDetail />} />
         <Route path="/new-quote" element={<NewQuote />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
