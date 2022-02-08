d5a8e86a746308206893cae6a8f15782616b8a5d
kimminkyu <kmk50009@naver.com>
2022-02-08 13:45:07 +0900

    Add Comments

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,47 @@
-import { useState } from "react";
-
+import { useState, useEffect, useCallback } from "react";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
 import classes from "./Comments.module.css";
 import NewCommentForm from "./NewCommentForm";
+import { useParams } from "react-router-dom";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import CommentsList from "./CommentsList";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
-
+  const params = useParams();
+  const { quoteId } = params;
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId)
+  },[sendRequest,quoteId]);
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (status === "completed" && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (
+    status === "completed" &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className="centered">No comments were added yet</p>;
+  }
 
   return (
     <section className={classes.comments}>
@@ -18,8 +51,13 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={quoteId}
+          onAddedComment={addedCommentHandler}
+        />
+      )}
+      {comments}
     </section>
   );
 };
